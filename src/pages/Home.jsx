81e59155b6288e71ee8abda750e7fd0cd7cc2aa6
yaasiinaxmed <yaskassoy@gmail.com>
2023-11-07 14:05:14 +0300

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Nav from '../components/Nav'
 import Hero from '../components/Hero'
-import Services from '../components/Services'
 
 function Home() {
   return (
@@ -9,7 +8,6 @@ function Home() {
         <div className='w-full h-full absolute inset-0 bg-img bg-center bg-cover'>
             <Nav/>
             <Hero/>
-            <Services/>
         </div>
     </div>
   )
